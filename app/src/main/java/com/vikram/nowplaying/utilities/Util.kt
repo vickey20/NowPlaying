caf6b05b2a9252101e70950fe31a257f9a20e956
Vikram Gupta <vikram.gupta@S02291-MBPR.local>
2018-10-01 14:27:05 -0700

    updates margins and text size, moved strings from class to res, refactored code for getting layman time

--- app/src/main/java/com/vikram/nowplaying/utilities/Util.kt
+++ app/src/main/java/com/vikram/nowplaying/utilities/Util.kt
@@ -8,6 +8,7 @@ import android.location.Location
 import android.support.v4.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Tasks
+import com.vikram.nowplaying.R
 
 private const val second = 1
 private const val minute = second * 60
@@ -36,32 +37,40 @@ fun getYears(seconds: Long): Long {
     return seconds / year
 }
 
-fun Long.laymanTime(): String {
+fun Long.laymanTime(context: Context): String {
     var curr = System.currentTimeMillis()
     var elapsedTime = (curr - this) / 1000
     return when (elapsedTime) {
-        in 0..minute -> "moments ago"
+        in 0..minute -> "${getString(context, R.string.moments_ago)}"
         in minute..hour -> {
             val minutes = getMinutes(elapsedTime)
-            return if (minutes > 1) "$minutes mins ago" else "$minutes min ago"
+            return if (minutes > 1) "$minutes ${getString(context, R.string.mins_ago)}" else "$minutes ${getString(context, R.string.min_ago)}"
         }
         in hour..day -> {
             val hours = getHours(elapsedTime)
-            return if (hours > 1) "$hours hrs ago" else "$hours hr ago"
+            return if (hours > 1) "$hours ${getString(context, R.string.hours_ago)}" else "$hours ${getString(context, R.string.hour_ago)}"
         }
         in day..month -> {
             val days = getDays(elapsedTime)
-            return if (days > 1) "$days days ago" else "$days day ago"
+            return if (days > 1) "$days ${getString(context, R.string.days_ago)}" else "$days ${getString(context, R.string.day_ago)}"
         }
         in day..year -> {
             val months = getMonths(elapsedTime)
-            return if (months > 1) "$months months ago" else "$months month ago"
+            return if (months > 1) "$months ${getString(context, R.string.months_ago)}" else "$months ${getString(context, R.string.month_ago)}"
         }
         else -> {
             val years = getYears(elapsedTime)
-            return if (years > 1) "$years years ago" else "$years year ago"
+            return if (years > 1) "$years ${getString(context, R.string.years_ago)}" else "$years ${getString(context, R.string.year_ago)}"
         }
     }
+}
+
+fun getString(context: Context, resId: Int): String {
+    return context.getString(resId)
+}
+
+fun getLaymanTime(context: Context, timestamp: Long): String {
+    return timestamp.laymanTime(context)
 }
 
 fun String.splitIntoTitleAndArtist(): List<String> {
